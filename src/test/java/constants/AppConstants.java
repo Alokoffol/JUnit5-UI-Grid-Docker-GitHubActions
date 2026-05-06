1886bec4d4b2a57fb5ee2fa9a800a5df0a6b2ed8
Олег <149000876+Alokoffol@users.noreply.github.com>
2026-05-06 10:35:34 +0300

    Update AppConstants.java
    Del com

--- src/test/java/constants/AppConstants.java
+++ src/test/java/constants/AppConstants.java
@@ -3,7 +3,6 @@ package constants;
 public class AppConstants {
     public static final String BASE_URL = "https://www.saucedemo.com";
 
-    // Таймауты лучше именовать конкретно
     public static final int IMPLICIT_WAIT_TIMEOUT = 10;
 
 }
